9f5edbbed7b1e524d8a0e430c529ee033b265084
Koh Meng Wang <mengwangk@gmail.com>
2018-02-21 22:29:01 +0800

    Updated files

---
+++ tests/trading/lesson_7_1.py
@@ -0,0 +1,46 @@
+"""Scatterplots."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from utils import get_data, plot_data, compute_daily_returns
+
+
+def test_run():
+    # Define a date range
+    dates = pd.date_range('2017-01-01', '2018-01-10')
+
+    # Choose stock symbols to read
+    symbols = ['SPY', 'AAPL', 'DXC']
+
+    # Get stock data
+    df = get_data(symbols, dates)
+    # plot_data(df)
+
+    # Computer daily returns
+    daily_returns = compute_daily_returns(df)
+
+    # Scatterplot SPY vs DXC
+    daily_returns.plot(kind='scatter', x='SPY', y='DXC')
+    beta_DXC, alpha_DXC = np.polyfit(daily_returns['SPY'], daily_returns['DXC'], 1)
+    print('beta_DXC', beta_DXC)
+    print('alpha_DXC', alpha_DXC)
+    plt.plot(daily_returns['SPY'], beta_DXC * daily_returns['SPY'] + alpha_DXC, '-', color='r')
+    plt.show()
+
+    # Scatterplot SPY vs AAPL
+    daily_returns.plot(kind='scatter', x='SPY', y='AAPL')
+    beta_AAPL, alpha_AAPL = np.polyfit(daily_returns['SPY'], daily_returns['AAPL'], 1)
+    print('beta_AAPL', beta_AAPL)
+    print('alpha_AAPL', alpha_AAPL)
+    plt.plot(daily_returns['SPY'], beta_AAPL * daily_returns['SPY'] + alpha_AAPL, '-', color='r')
+
+    plt.show()
+
+    # Calculate correlation coefficient
+    print(daily_returns.corr(method='pearson'))
+
+
+if __name__ == "__main__":
+    test_run()
