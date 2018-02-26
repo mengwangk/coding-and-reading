83a7b9fca078f445f3153708a40f43c5a55c9cf5
Koh Meng Wang <mengwangk@gmail.com>
2018-02-27 00:21:47 +0800

    Updated files

--- tests/trading/lesson_8_1.py
+++ tests/trading/lesson_8_1.py
@@ -28,6 +28,19 @@ def f(X):
 
 
 def test_run():
+    Xguess = 2.0
+    min_result = spo.minimize(f, Xguess, method='SLSQP', options={'disp': True})
+    print("Minima found at: ")
+    print("X = {}, Y = {}".format(min_result.x, min_result.fun))
+
+    # Plot function values, mark minima
+    Xplot = np.linspace(0.5, 2.5, 21)
+    Yplot = f(Xplot)
+    plt.plot(Xplot, Yplot)
+    plt.plot(min_result.x, min_result.fun, 'ro')    # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html
+    plt.title("Minima of an objective function")
+    plt.show()
+
 
 if __name__ == "__main__":
     test_run()
