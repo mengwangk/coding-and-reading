1673b0e38f0c4f783f432f3cc9c07277273f6b4c
Koh Meng Wang <mengwangk@gmail.com>
2018-03-01 19:20:15 +0800

    Updated files

---
+++ tests/trading/lesson_8_2.py
@@ -0,0 +1,53 @@
+"""
+Convex problem - a real-valued function f(x) defined on an interval is called convex
+if the line segment between any two points on the graph of the function lies above the graph ..
+
+Parameterized model - f(x) = c0x + c1
+
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as spo
+
+
+def error(line, data):  # error function
+    """Compute error between given line model and observed data
+
+    Parameters
+    ----------
+    line: tuple/list/array (C0, C1) where C0 is slope and C1 is Y-intercept
+    data: 2D array where each row is a point (x,y)
+
+    Returns error as single real value
+    """
+    # Metric: Sum of squared Y-axis differences
+    err = np.sum((data[:, -1] - (line[0] * data[:, 0] + line[1])) ** 2)
+    return err
+
+
+def f(X):
+    """Given a salar X, return some value (a real number)."""
+    Y = (X - 1.5) ** 2 + 0.5
+    print("X = {}, Y = {}".format(X, Y))  # for tracing
+    return Y
+
+
+def test_run():
+    Xguess = 2.0
+    min_result = spo.minimize(f, Xguess, method='SLSQP', options={'disp': True})
+    print("Minima found at: ")
+    print("X = {}, Y = {}".format(min_result.x, min_result.fun))
+
+    # Plot function values, mark minima
+    Xplot = np.linspace(0.5, 2.5, 21)
+    Yplot = f(Xplot)
+    plt.plot(Xplot, Yplot)
+    plt.plot(min_result.x, min_result.fun, 'ro')  # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html
+    plt.title("Minima of an objective function")
+    plt.show()
+
+
+if __name__ == "__main__":
+    test_run()
