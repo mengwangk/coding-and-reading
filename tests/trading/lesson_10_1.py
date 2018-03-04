3c11d3b56778545bff907b20818948306dd1ff75
Koh Meng Wang <mengwangk@gmail.com>
2018-03-04 21:34:14 +0800

    Updated file

---
+++ tests/trading/lesson_10_1.py
@@ -0,0 +1,23 @@
+"""
+ETF - Exchanged traded fund
+- Buy/sell like stocks
+- Baskets of stocks
+- Transparent
+- Liquid
+
+
+Mutual fund
+- Buy/sell at end of day
+- Quarterly disclouse
+- Less transparent
+
+Hedge fund
+- Buy/sell by agreement
+- No disclosure
+- Not transparent
+
+Incentives for fund managers
+----------------------------
+
+
+"""
